e391d5bf77623c3a9315cbfe81a7c0bbc23f9434
Emily Grace Acres <acres.emilygrace@gmail.com>
2017-12-16 15:44:02 -0500

    chore: animate scroll arrow

--- public/animations.js
+++ public/animations.js
@@ -1,9 +1,18 @@
-$( document ).ready(function() {
-  $( "#nav-filter" ).click(function() {
+$( document ).ready(function () {
+  $( "#nav-filter" ).click(function () {
     $("#filters").toggle();
   });
 
-  $('#down-arrow').hover(function() {
+  $('#down-arrow').hover(function () {
 
   });
+
+  $('#down-arrow').click(function () {
+    var height = $( window ).height();
+    $('html, body').animate({scrollTop: height},'50');
+  })
+
+  $('#nav-logo').click(function () {
+    $('html, body').animate({scrollTop: 0},'50');
+  })
 })
