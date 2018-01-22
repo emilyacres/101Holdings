d738262dcc53b4ecdae35253d3537850b9f42949
Emily Grace Acres <acres.emilygrace@gmail.com>
2018-01-22 15:02:40 -0500

    fix: db still not connecting

--- server/db/db.js
+++ server/db/db.js
@@ -4,16 +4,16 @@ const Sequelize = require('sequelize')
 //     logging: false
 //   }
 // )
-let db;
+
 if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
-  db = new Sequelize(process.env.DATABASE_URL, {
+  global.db = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
     logging:  true //false
   });
 } else {
   // the application is executed on the local machine
-  db = new Sequelize("postgres://localhost:5432/oneohone");
+  global.db = new Sequelize("postgres://localhost:5432/oneohone");
 }
-module.exports = db
+module.exports = global.db
