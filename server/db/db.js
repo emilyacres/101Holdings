f0745152f3e5946d240649bb73ee2259b19af782
Emily Grace Acres <acres.emilygrace@gmail.com>
2018-01-22 14:56:55 -0500

    fix: db unconnected

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,19 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/oneohone', {
-    logging: false
-  }
-)
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || 'postgres://localhost:5432/oneohone', {
+//     logging: false
+//   }
+// )
+let db;
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  db = new Sequelize("postgres://localhost:5432/oneohone");
+}
 module.exports = db
