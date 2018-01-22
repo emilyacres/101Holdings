e48f549b123640b9bd42c0680aa5044ee2bec9de
Emily Grace Acres <acres.emilygrace@gmail.com>
2018-01-22 16:36:19 -0500

    fix: db

--- server/db/db.js
+++ server/db/db.js
@@ -5,9 +5,13 @@ const Sequelize = require('sequelize')
 //   }
 // )
 
-if (process.env.DATABASE_URL) {
+var isProduction = process.env.NODE_ENV == 'production' ? true : false
+
+
+var connectionString = 'postgres://oqjlkcwyzndolb:9a1e189c8be091a9a47d76e18e71a0659504b030f855c4a0313456253e795fc3@ec2-174-129-22-84.compute-1.amazonaws.com:5432/d7pbvkurlik213'
+if (isProduction) {
   // the application is executed on Heroku ... use the postgres database
-  var db = new Sequelize(process.env.DATABASE_URL, {
+  var db = new Sequelize(connectionString, {
     dialect:  'postgres',
     protocol: 'postgres',
     logging:  false //true
